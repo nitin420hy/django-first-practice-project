3decee2889688e3a47ef50c1dbe29df3750693f9
Nitin <nitinchauhan.bsiotr@gmail.com>
2021-03-23 19:59:11 +0530

    test.html file is rendered showTest() of views.py in exam folder

--- exam/views.py
+++ exam/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def showTest(request):
-    s="<p>This is Show Test Page</p>"
-    return HttpResponse(s)
+    res=render(request,'exam/test.html')
+    return res
 
 def showResult(request):
     s="<p>This is show result page</p>"
