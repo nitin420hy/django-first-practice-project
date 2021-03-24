69bbda30de561f6cb9efbc816998e303f2c3fe58
Nitin <nitinchauhan.bsiotr@gmail.com>
2021-03-24 09:33:28 +0530

    added template variable and template tag

--- exam/views.py
+++ exam/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def showTest(request):
-    res=render(request,'exam/test.html')
+    que="Who developed C Language?"
+    a="Ken Thompson"
+    b="Dennis Ritchie"
+    c="Bjarne Stroustrup"
+    d="Saurabh Shukla"
+    level="Level defined"
+    data={'que':a,'a':a,'b':b,'c':c,'d':d,'level':level}
+    res=render(request,'exam/test.html',context=data)
     return res
 
 def showResult(request):
